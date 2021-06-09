a93b45dd28c2105c9242e09d01666e9eef21e88d
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2021-06-09 09:21:33 -0400

    Add files via upload

---
+++ src/Questions.java
@@ -0,0 +1,15 @@
+
+public class Questions {
+	public static void main(String[] args)
+    {
+        String myName = 
+        int luckyNumber =
+        double currentTemperature =
+        boolean isStudent = 
+        
+        System.out.println(myName);
+        System.out.println(luckyNumber);
+        System.out.println(currentTemperature);
+        System.out.println(isStudent);
+    }
+}
